d6bc93ea5de57edd5a199d56e6094c51d4affee3
Ahmad Kaddoura <kaddoura.ahmad@gmail.com>
2024-01-14 22:49:40 -0800

    Initial Commit

---
+++ ViewMods/ViewModsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModsApp.swift
+//  ViewMods
+//
+//  Created by ahmad kaddoura on 1/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewModsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
